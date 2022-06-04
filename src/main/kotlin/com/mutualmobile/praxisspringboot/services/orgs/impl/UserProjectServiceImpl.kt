89fcf24e62e40282fd30c65a994b7f25c9dffbcd
Anmol Verma <anmol.verma4@gmail.com>
2022-06-04 20:43:52 +0530

    send name in response instead of id's

--- src/main/kotlin/com/mutualmobile/praxisspringboot/services/orgs/impl/UserProjectServiceImpl.kt
+++ src/main/kotlin/com/mutualmobile/praxisspringboot/services/orgs/impl/UserProjectServiceImpl.kt
@@ -3,12 +3,17 @@ package com.mutualmobile.praxisspringboot.services.orgs.impl
 import com.mutualmobile.praxisspringboot.data.ApiResponse
 import com.mutualmobile.praxisspringboot.data.models.projects.HarvestUserWork
 import com.mutualmobile.praxisspringboot.data.user.HarvestUserProjectAssignment
+import com.mutualmobile.praxisspringboot.entities.orgs.DBOrgProjects
 import com.mutualmobile.praxisspringboot.entities.projects.DBUserWork
+import com.mutualmobile.praxisspringboot.entities.user.DBHarvestUser
 import com.mutualmobile.praxisspringboot.entities.user.DBUserProjectAssignment
 import com.mutualmobile.praxisspringboot.repositories.UserProjectRepository
+import com.mutualmobile.praxisspringboot.repositories.UserRepository
+import com.mutualmobile.praxisspringboot.repositories.orgs.OrgProjectsRepository
 import com.mutualmobile.praxisspringboot.repositories.orgs.UserWorkRepository
 import com.mutualmobile.praxisspringboot.services.orgs.UserProjectService
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.data.repository.findByIdOrNull
 import org.springframework.stereotype.Service
 
 @Service
@@ -17,12 +22,18 @@ class UserProjectServiceImpl : UserProjectService {
     lateinit var userProjectRepository: UserProjectRepository
 
     @Autowired
+    lateinit var projectsRepository: OrgProjectsRepository
+
+    @Autowired
+    lateinit var userRepository: UserRepository
+
+    @Autowired
     lateinit var userWorkRepository: UserWorkRepository
 
     override fun assignProjectsToUsers(workList: HashMap<String, List<String>>): ApiResponse<Unit> {
         return try {
             val workToSave = mutableListOf<HarvestUserProjectAssignment>()
-            val errorProjectAssignmentIds = mutableListOf<Pair<String, String>>()
+            val errorProjectAssignmentIds = mutableListOf<Pair<DBOrgProjects, DBHarvestUser>>()
             workList.forEach { (projectId, userIds) ->
                 userIds.forEach { userId ->
                     val doesProjectAssignmentExist = userProjectRepository.existsByProjectIdAndUserId(projectId, userId)
@@ -30,10 +41,16 @@ class UserProjectServiceImpl : UserProjectService {
                     if (!doesProjectAssignmentExist) {
                         workToSave.add(HarvestUserProjectAssignment(projectId = projectId, userId = userId))
                     } else {
-                        val errorPair = Pair(projectId, userId)
+                        val errorPair =
+                            Pair(projectsRepository.findByIdOrNull(projectId), userRepository.findByIdOrNull(userId))
 
                         if (!errorProjectAssignmentIds.contains(errorPair)) {
-                            errorProjectAssignmentIds.add(errorPair)
+                            errorPair.first?.let { project->
+                                errorPair.second?.let { user->
+                                    errorProjectAssignmentIds.add(Pair(project,user))
+                                }
+                            }
+
                         }
                     }
                 }
@@ -42,7 +59,7 @@ class UserProjectServiceImpl : UserProjectService {
             if (errorProjectAssignmentIds.isNotEmpty()) {
                 throw Exception(
                     "The correct assignments were made successfully but the following project-user assignments already exist: ${
-                        errorProjectAssignmentIds.joinToString { "ProjectId:${it.first} - UserId:${it.second}" }
+                        errorProjectAssignmentIds.joinToString { "ProjectId:${it.first.name} - UserId:${it.second.name()}" }
                     }"
                 )
             }
