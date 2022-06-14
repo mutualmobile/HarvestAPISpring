acb67db40547dd58fa7271ff12d3fb27091fce15
Yugesh Jain <yugesh.jain@mutualmobile.com>
2022-06-14 14:33:32 +0530

    update - added a default value to workType

--- src/main/kotlin/com/mutualmobile/praxisspringboot/data/models/projects/HarvestUserWork.kt
+++ src/main/kotlin/com/mutualmobile/praxisspringboot/data/models/projects/HarvestUserWork.kt
@@ -10,7 +10,7 @@ data class HarvestUserWork(
     val userId: String,
     val workDate: Date,
     val workHours: Float,
-    val workType: WorkType,
+    val workType: WorkType = WorkType.BILLABLE,
     val note: String? = null
 )
 
